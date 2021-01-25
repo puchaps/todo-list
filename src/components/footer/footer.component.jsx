d2428ef1a7aa758ff15443a67111a431056b2aa0
Pasha Adamovich <puchapse@gmail.com>
2021-01-25 17:47:27 +0200

    App adapted for mobile devices

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="foooter-icon">
           <a href="https://github.com/puchaps">
             <img 
-              src="https://www.flaticon.com/svg/vstatic/svg/889/889111.svg?token=exp=1611263644~hmac=e6264c798c4a351d1661318e50bfb8c6"
+              src="https://www.flaticon.com/svg/vstatic/svg/733/733553.svg?token=exp=1611589484~hmac=bc1be3c80f0aa311c2337c729f01c08e"
               alt="gihub"
             />
           </a>
@@ -15,7 +15,7 @@ const Footer = () => {
         <div className="foooter-icon">
           <a href="https://www.instagram.com/adamovich_ps/?igshid=2rl7e5y71xkf">
             <img 
-              src="https://www.flaticon.com/svg/vstatic/svg/2111/2111463.svg?token=exp=1611263841~hmac=50bb8ca61645d97bb2e85c1189b93788"
+              src="https://www.flaticon.com/svg/vstatic/svg/733/733558.svg?token=exp=1611589535~hmac=742dc640a5e7d0928ebf823656a4192c"
               alt="instagram"
             />
           </a>
